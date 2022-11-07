3a152b62b82549ad67537d88c950b52fea1f9156
ClarenceEMoore <>
2022-11-06 16:56:04 -0800

    Updating the Details activity layout with information relative to country selected

--- CountriesInfoApp/app/src/main/java/com/eaglewarrior/countriesinfoapp/DetailsActivity.kt
+++ CountriesInfoApp/app/src/main/java/com/eaglewarrior/countriesinfoapp/DetailsActivity.kt
@@ -10,7 +10,14 @@ class DetailsActivity : AppCompatActivity() {
 
     // Declaring Layout Variables
     private lateinit var countryNameTv: TextView
-
+    private lateinit var capitalCityTv: TextView
+    private lateinit var populationTv: TextView
+    private lateinit var regionTv: TextView
+    private lateinit var subRegionTv: TextView
+    private lateinit var areaTv: TextView
+    private lateinit var currenciesTv: TextView
+    private lateinit var languageTv: TextView
+    private lateinit var neighboringCountriesTv: TextView
     // Declaring Variables
 
     private lateinit var adapter: CountriesAdapter
@@ -26,6 +33,30 @@ class DetailsActivity : AppCompatActivity() {
 
         // Initializing Variables
         countryNameTv = findViewById(R.id.country_name_tv)
-        countryNameTv.text = currentCountry?.name
+        countryNameTv.text = currentCountry?.nativeName
+
+        regionTv = findViewById(R.id.region_tv)
+        regionTv.text = currentCountry?.region
+
+        subRegionTv = findViewById(R.id.sub_region_tv)
+        subRegionTv.text = currentCountry?.region
+
+        capitalCityTv = findViewById(R.id.capital_city_tv)
+        capitalCityTv.text = currentCountry?.capital
+
+        languageTv = findViewById(R.id.languages_tv)
+        languageTv.text = currentCountry?.languages?.joinToString(", ")
+
+        currenciesTv = findViewById(R.id.currencies_tv)
+        currenciesTv.text = currentCountry?.currencies?.joinToString(", ")
+
+        areaTv = findViewById(R.id.area_tv)
+        areaTv.text = currentCountry?.area.toString()
+
+        populationTv = findViewById(R.id.population_tv)
+        populationTv.text = currentCountry?.population?.toString()
+
+        neighboringCountriesTv = findViewById(R.id.neighboring_countries_tv)
+        neighboringCountriesTv.text = currentCountry?.borders?.joinToString(", ")
 
     }}
